df9c71160a18621b0457d57c1e72d00073d4ba42
Oppilaal Daniel <doppilaal11@gmail.com>
2023-09-19 19:58:00 +0530

    Add files via upload
    The PowerShell and Automation JSON file helps the user understand the concept of Unpublishing a Published Application from Managed Space. It can be done by different methods
    1. Using API Key in PowerShell
    2. Using Qlik-CLI in PowerShell
    3. Using API Requests in (QAA) Qlik Application Automation

---
+++ Unpublish an Application from Managed Space.ps1
@@ -0,0 +1,8 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$body='{"spaceId":""}'
+$hdrs = @{}
+$hdrs.Add("Authorization","Bearer eyJhbGciOiJFUzM4NCIsImtpZCI6IjNiNDI5NDU4LThmYjEtNDIwZC1hOWEzLTFjYzYyOGU4YTM1ZSIsInR5cCI6IkpXVCJ9.eyJzdWJUeXBlIjoidXNlciIsInRlbmFudElkIjoibVhONnplVUNHMUdIZ0FXLXdoUFpaMkkzMUJ5aXRQRDMiLCJqdGkiOiIzYjQyOTQ1OC04ZmIxLTQyMGQtYTlhMy0xY2M2MjhlOGEzNWUiLCJhdWQiOiJxbGlrLmFwaSIsImlzcyI6InFsaWsuYXBpL2FwaS1rZXlzIiwic3ViIjoiUlA3X3dUbko1MGNYdHBBWFhIWFVJYnVsRnhtMnVBYWUifQ.4XMEPk6chZ3N7TSDN0RTjDHd2PNUsp9Vq7dOx7SuHecMQsHOUVaPiXGwFA1RhNgX0Gr0_hoOUKzby0JUlaYO8i6ksPwkAKH0ruExzbvZult5xMpiZ9EVjBsddI5Oe7iZ")
+$hdrs.Add("content-type","application/json")
+$url = "https://gisna.us.qlikcloud.com/api/v1/apps/88db1ea8-f2d3-4490-991e-e0d55c23b75a/space"
+Invoke-RestMethod -Uri $url -Method Put -Headers $hdrs -Body $body
