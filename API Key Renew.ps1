6d24ce401d2b5338f4246954498c499698c8f1d3
Oppilaal Daniel <doppilaal11@gmail.com>
2023-09-19 19:18:11 +0530

    Add files via upload
    This PowerShell script helps the user, generate API Keys using an API Key in Qlik SaaS. This can be done with multiple methods.
    1. Using API Key in PowerShell
    2. Using API Key and Qlik-CLI

---
+++ API Key Renew.ps1
@@ -0,0 +1,8 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$body='{"description":**1**,"expiry":**2**}'
+$hdrs = @{}
+$hdrs.Add("Authorization","Bearer **3**")
+$hdrs.Add("content-type","application/json")
+$url = "https://**4**/api/v1/api-keys"
+Invoke-RestMethod -Uri $url -Method Post -Headers $hdrs -Body $body
